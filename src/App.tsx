5c8f35d093fd89edffb104e23bccb70b2f41eed6
Theodore Balashov <theodore.balashov@revature.com>
2023-10-13 14:35:00 -0400

    painting app complete

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AllPaintingsPage } from './pages/AllPaintingsPage';
 import { SubmitPaintingPage } from './pages/SubmitPaintingPage';
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar></Navbar>
       <Routes>
-        <Route path="list" element={<AllPaintingsPage></AllPaintingsPage>}></Route>
-        <Route path="submit" element={<SubmitPaintingPage></SubmitPaintingPage>}></Route>
+        
+        <Route path="/submit" element={<SubmitPaintingPage></SubmitPaintingPage>}></Route>
+        <Route path="*" element={<AllPaintingsPage></AllPaintingsPage>}></Route>
       </Routes>
       </BrowserRouter>
     </div>
